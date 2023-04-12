e1460849e5c70a3ca031e26864714af8cdb026b8
Ben Longaker <119361112+benjo7474@users.noreply.github.com>
2023-04-12 00:10:46 -0600

    Add files via upload

---
+++ Ben's Code/Kappa_Level_Curves/Level_Curve_k/Kappa_level_curves_k.m
@@ -0,0 +1,42 @@
+kplot = 0.1:0.01:1;
+count=1;
+initial_guesses = zeros(1,length(kplot));
+gamma_xie = zeros(1,length(kplot));
+gamma_root = zeros(1,length(kplot));
+
+kappa = 1;
+theta = 1;
+mu = 0;
+
+tic;
+
+for k=kplot
+    init_guess = Vlasov_1D_linearized_Steve_v3_Kappa(k, theta, mu, kappa);
+    initial_guesses(count) = init_guess;
+    gamma_xie(count) = Kappa_Disp_Using_Xie(k, theta, mu, kappa, init_guess);
+    gamma_root(count) = dielectric(k,theta,theta,mu,mu,1,init_guess);
+    count = count+1;
+end
+
+toc
+
+% Create Plots
+figure
+plot(kplot, imag(initial_guesses))
+hold on
+plot(kplot, imag(gamma_xie))
+plot(kplot, imag(gamma_root))
+xlabel('$k$','Interpreter','latex','FontSize',16)
+ylabel('$\gamma(k)$','Interpreter','latex','FontSize',16)
+title(['Kappa Distribution ($\kappa=',num2str(kappa),'$)'],'Interpreter','latex')
+legend('Spectral Method','Dispersion Relation with Xie Code','Root Finding with dielectric.m')
+
+figure
+plot(kplot, real(initial_guesses));
+hold on
+plot(kplot, real(gamma_xie));
+plot(kplot, real(gamma_root));
+xlabel('$k$','Interpreter','latex','FontSize',16)
+ylabel('$\Omega(k)$','Interpreter','latex','FontSize',16)
+title('Real Part of Initial Guess and Xie')
+legend('Spectral Method','Dispersion Relation with Xie Code','Root Finding with dielectric.m')
