6905785fc624cf549d91e515a0a63631914f53d6
npalmer34 <144279151+npalmer34@users.noreply.github.com>
2023-11-13 11:25:50 -0700

    README, zetaph in IMax folder is IMax specific

--- Active_Subspaces/1D/IncompleteMax/IncompleteMax_Disp_Using_Xie.m
+++ Active_Subspaces/1D/IncompleteMax/IncompleteMax_Disp_Using_Xie.m
@@ -8,7 +8,7 @@ function xi = IncompleteMax_Disp_Using_Xie(k, sigma, mu, nu, init_guess)
 
     Fn = 5; % 5 = option for incomplete maxwellian
     Nfourier = 2000; % number of Fourier coefficients to take
-    D = @(z) 1-1/k^2*zetaph(z, Fn, F, Nfourier, mu, sigma, nu); % dielectric function
+    D = @(z) 1-1/k^2*zetaph_IMax(z, Fn, F, Nfourier, mu, sigma, nu); % dielectric function
 
     xi = fsolve(D, init_guess, options); % root finder
 
